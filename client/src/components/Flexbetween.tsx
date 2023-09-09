2ea3269a02ba593b95a7f448d64312ea49353113
rorense <rkorense@gmail.com>
2023-09-09 16:25:03 +1200

    Updated with comment

--- client/src/components/Flexbetween.tsx
+++ client/src/components/Flexbetween.tsx
@@ -1,6 +1,7 @@
 import { Box } from "@mui/material";
 import { styled } from "@mui/system";
 
+// Reusuable flex box component
 const FlexBetween = styled(Box)({
     display: "flex",
     justifyContent: "space-between",
