4c315a64166ab24f761dbb5b3dad26a464428760
rorense <rkorense@gmail.com>
2023-09-11 21:51:57 +1200

    working on dashboard

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,9 +1,39 @@
 import { Box, useTheme } from '@mui/material';
 
+const gridTemplate = `
+    "a b c"
+    "a b c"
+    "a b c"
+    "a b f"
+    "d e f"
+    "d e f"
+    "d h i"
+    "g h i"
+    "g h j"
+    "g h j"
+`
+
 function Dashboard() {
-    const { palette } = useTheme();
+    useTheme();
   return (
-    <Box color={ palette.grey[300]}>Dashboard</Box>
+    <Box width="100%" height="100%" display="grid" gap="1.5rem"
+        sx={{
+            gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+            gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+            gridTemplateAreas: gridTemplate,
+        }}>
+        <Box gridArea="a"></Box>
+        <Box gridArea="b"></Box>
+        <Box gridArea="c"></Box>
+        <Box gridArea="d"></Box>
+        <Box gridArea="e"></Box>
+        <Box gridArea="f"></Box>
+        <Box gridArea="g"></Box>
+        <Box gridArea="h"></Box>
+        <Box gridArea="i"></Box>
+        <Box gridArea="j"></Box>
+    </Box>
+
   )
 }
 
