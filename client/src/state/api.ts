b7281fc052c10ab2d0c370efef69eb267380356f
rorense <rkorense@gmail.com>
2023-09-19 15:39:19 +1200

    Setting up back-end server

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -7,6 +7,7 @@ export const api = createApi({
     endpoints: (build) => ({
         getKpis: build.query<void, void>({
             query: () => "kpi/kpis/",
+            // @ts-expect-error dddd
             providesTags: ["Kpis"]
         }),
     }),
