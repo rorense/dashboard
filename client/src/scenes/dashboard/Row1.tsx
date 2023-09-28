16062a1e8a95ff1709f521abc70717d05fc7f12b
rorense <rkorense@gmail.com>
2023-09-29 10:11:48 +1300

    Working on 3rd chart

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -3,7 +3,7 @@ import DashboardBox from '@/components/DashboardBox'
 import { useGetKpisQuery } from '@/state/api'
 import { useTheme } from '@mui/material';
 import { useMemo } from 'react'
-import { Area, AreaChart, CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Row1 = () => {
   // Importing data from KPIs
@@ -163,8 +163,35 @@ const Row1 = () => {
           </ResponsiveContainer>
         </DashboardBox>
 
-
-        <DashboardBox gridArea="c"></DashboardBox>
+        {/* Chart 3 */}
+        <DashboardBox gridArea="c">
+          <BoxHeader 
+            title="Revenue Month by Month"
+            subtitle='top line represents revenue, bottom line represents expenses'
+            sideText='+4%'
+          />  
+          <ResponsiveContainer width="100%" height="100%">
+            <BarChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="pv" fill="#8884d8" />
+            <Bar dataKey="uv" fill="#82ca9d" />
+            </BarChart>
+          </ResponsiveContainer>
+        </DashboardBox>
     </>
   )
 }
