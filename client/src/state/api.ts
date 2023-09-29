18442c176ee747341ecfb58b927135874a143cb8
rorense <rkorense@gmail.com>
2023-09-30 12:01:23 +1300

    Redux

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { GetKpisResponse, GetProductsResponse } from "./types";
 
+// Creating api using redux
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
     reducerPath: "main",
