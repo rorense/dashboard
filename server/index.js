fdbf7bfbcde7965030e367c9f210521401ca9eee
rorense <rkorense@gmail.com>
2023-10-06 10:47:49 +1300

    Working on transaction routes

--- server/index.js
+++ server/index.js
@@ -7,9 +7,11 @@ import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js";
 import KPI from "./models/KPI.js";
-import { kpis, products } from "./data/data.js";
+import { kpis, products, transactions } from "./data/data.js";
 import productRoutes from "./routes/product.js";
 import Product from "./models/Product.js";
+import transactionRoutes from "./routes/transaction.js";
+import Transaction from "./models/Transaction.js"
 
 // Configuration of backend tools.
 dotenv.config();
@@ -24,7 +26,8 @@ app.use(cors());
 
 // Setting up routes
 app.use("/kpi", kpiRoutes);
-app.use("/product", productRoutes)
+app.use("/product", productRoutes);
+app.use("/transaction", transactionRoutes);
 
 // mongoose setup
 const PORT = process.env.PORT || 9000;
