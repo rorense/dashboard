b5f4254fddf980b92adcc19b902f43b43885f75a
rorense <rkorense@gmail.com>
2023-10-06 11:32:35 +1300

    working on first chart

--- client/src/scenes/dashboard/Row3.tsx
+++ client/src/scenes/dashboard/Row3.tsx
@@ -1,16 +1,60 @@
+import BoxHeader from '@/components/BoxHeader';
 import DashboardBox from '@/components/DashboardBox'
-import { useGetTransactionsQuery } from '@/state/api'
+import { useGetKpisQuery, useGetProductsQuery, useGetTransactionsQuery } from '@/state/api'
+import { Box, useTheme } from '@mui/material';
+import { DataGrid, GridCellParams } from '@mui/x-data-grid';
 
 function Row3() {
+  const { palette } = useTheme();
+  const { data: productData } = useGetProductsQuery();
+  const { data: transactionData } = useGetTransactionsQuery();
+  const { data: kpiData } = useGetKpisQuery();
 
-  const { data: transactionsData } = useGetTransactionsQuery();
-  console.log("transactionsData:", transactionsData)
+  const productColumns = [
+    {
+      field: "_id",
+      headername: "id",
+      flex: 1,
+    },
+    {
+      field: "expense",
+      headername: "Expense",
+      flex: 0.5,
+      renderCell: (params: GridCellParams) => `$${params.value}`,
+    },
+    {
+      field: "price",
+      headername: "Price",
+      flex: 0.5,
+      renderCell: (params: GridCellParams) => `$${params.value}`,
+    },
+  ]
 
   return (
     <>
-      <DashboardBox gridArea="g"></DashboardBox>
+    {/* First Chart */}
+      <DashboardBox gridArea="g">
+        <BoxHeader title="List of Products" sideText={`${productData?.length} products`}/>
+        <Box 
+          mt="0.5rem"
+          p="0 0.5rem"
+          height="75%"
+          sx={{
+            "& .MuiDataGrid-root": {
+              color: palette.grey[300],
+              border: "none",
+            },
+          }}>
+          <DataGrid 
+            columns={productColumns} 
+            rows={productData || [] } />
+        </Box>
+      </DashboardBox>
+
       <DashboardBox gridArea="h"></DashboardBox>
+
       <DashboardBox gridArea="i"></DashboardBox>
+
       <DashboardBox gridArea="j"></DashboardBox>
     </>
   )
